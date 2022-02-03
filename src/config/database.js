cf8487facbef48edbe6eed091747df227432805d
ElderSam <eldersamuel98@gmail.com>
2022-02-03 08:08:00 -0300

    install dotenv & config environment variables (dev & test)

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,14 @@
+require("dotenv").config({
+    path: process.env.NODE_ENV === "test" ? ".env.test" : ".env"
+})
+
 module.exports = {
-	host: "127.0.0.1",
-	username: "postgres",
-	password: "admin",
-	database: "nodeauth-TDD",
-	dialect: "postgres",
+	host: process.env.DB_HOST,
+	username: process.env.DB_USER,
+	password: process.env.DB_PASS,
+	database: process.env.DB_NAME,
+	dialect: process.env.DB_DIALECT || "postgres",
+	storage: './__tests__/database.sqlite', // db for tests
 	// operatorsAliases: false, // configuração para desabilitar um warning do Sequelize
 	logging: false, //  não mostrar muitos logs enquando rodamos as migrations
   define: {

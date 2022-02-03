29adcc5c1028bf37b0fab6fe6a8ece0f2642fff9
ElderSam <eldersamuel98@gmail.com>
2022-02-03 19:56:39 -0300

    feat: add User model test (create user)

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,10 +1,17 @@
+const { User } = require('../../src/app/models');
+
 describe("Authentication", () => {
-	it("should sum two numbers", () => {
-		const x = 2;
-		const y = 4;
+	it("should create a user", async () => {
+		const emailTest = 'test@gmail.com.br'
 
-		const sum = x + y;
+		const user = await User.create({
+			name: 'Samuel',
+			email: emailTest,
+			password_hash: "123123"
+		})
 
-		expect(sum).toBe(6);
+		console.log(user)
+
+		expect(user.email).toBe(emailTest);
 	});
 });

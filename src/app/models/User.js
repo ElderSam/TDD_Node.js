d8b588ed361bc03cdd14dbc0dbc6f5b888fda879
ElderSam <eldersamuel98@gmail.com>
2022-02-03 21:33:35 -0300

    feat: User model - encrypt user password before save

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,15 +1,20 @@
+const bcrypt = require('bcryptjs');
+
 module.exports = (sequelize, DataTypes) => {
 	const User = sequelize.define(
 		"User",
 		{
 			name: DataTypes.STRING,
 			email: DataTypes.STRING,
+			password: DataTypes.VIRTUAL,
 			password_hash: DataTypes.STRING,
 		},
 		{
 			hooks: {
 				beforeSave: async (user) => {
-					user.password_hash = "123";
+					if(user.password) {
+						user.password_hash = await bcrypt.hash(user.password, 8)
+					}
 				},
 			},
 		}

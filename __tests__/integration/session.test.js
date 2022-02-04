eb30995e9f92b0cddfbedf14f9a5099ca3b9efd1
ElderSam <eldersamuel98@gmail.com>
2022-02-04 08:36:57 -0300

    test: route /dashboard

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -47,4 +47,14 @@ describe("Authentication", () => {
 
 		expect(response.body).toHaveProperty("token");
 	});
+
+	it("should be able to access private routes when authenticated", async () => {
+		const user = await factory.create("User");
+
+		const response = await request(app)
+			.get("/dashboard")
+			.set("Authorization", `Bearer ${user.generateToken()}`);
+
+		expect(response.status).toBe(200)
+	});
 });

cd3fde7b8fbd83c53582721da785a285b5ce8b14
ElderSam <eldersamuel98@gmail.com>
2022-02-04 08:37:20 -0300

    feat: create dashboard route

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,13 @@
 const routes = require("express").Router();
-const SessionController = require('./app/controllers/SessionController')
 
-// Definição das Rotas
+const SessionController = require('./app/controllers/SessionController')
+const authMiddleware = require('./app/middleware/auth')
+
+// Routes definition
 routes.post('/sessions', SessionController.store)
+
+routes.get('/dashboard', (req, res) => {
+    res.status(200).send()
+})
 
 module.exports = routes;

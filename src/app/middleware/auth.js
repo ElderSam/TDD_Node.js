dd6740eb0fcd4703853629abfd8d33f052f4401a
ElderSam <eldersamuel98@gmail.com>
2022-02-04 09:10:53 -0300

    feat: auth - validate jwt token

--- src/app/middleware/auth.js
+++ src/app/middleware/auth.js
@@ -1,11 +1,22 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const { promisify } = require("util");
 
-module.exports =(req, res, next) => {
-    const authHeader = req.headers.authorization;
+module.exports = async (req, res, next) => {
+	const authHeader = req.headers.authorization;
 
-    if(!authHeader) {
-        return res.status(401).json({ message: 'Token not privided' });
-    }
+	if (!authHeader) {
+		return res.status(401).json({ message: "Token not privided" });
+	}
 
-    return next();
+	const [, token] = authHeader.split(" ");
+
+	try {
+        const decoded = await promisify(jwt.verify)(token, process.env.APP_SECRET);
+
+		req.userId = decoded.id;
+
+		return next();
+	} catch (err) {
+		return res.status(401).json({ message: "Token invalid" });
+	}
 };

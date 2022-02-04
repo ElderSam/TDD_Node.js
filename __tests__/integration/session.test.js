826082b9a463118a8e6fbe6a9dbd45a4e88b1b61
ElderSam <eldersamuel98@gmail.com>
2022-02-03 21:01:35 -0300

    avoiding error when running tests by cleaning DB before each test

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,17 +1,11 @@
-const { User } = require('../../src/app/models');
+const truncate = require("../utils/truncate")
 
 describe("Authentication", () => {
-	it("should create a user", async () => {
-		const emailTest = 'test@gmail.com.br'
+	beforeEach(async () => {
+		await truncate();
+	})
 
-		const user = await User.create({
-			name: 'Samuel',
-			email: emailTest,
-			password_hash: "123123"
-		})
-
-		console.log(user)
-
-		expect(user.email).toBe(emailTest);
+	it("", async () => {
+		
 	});
 });

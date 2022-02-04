b1433f524ac1906b1b622f90daaa341af50966d8
ElderSam <eldersamuel98@gmail.com>
2022-02-03 21:16:21 -0300

    feat: simulating a hash (test fail)

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,9 +1,19 @@
 module.exports = (sequelize, DataTypes) => {
-    const User = sequelize.define("User", {
-        name: DataTypes.STRING,
-        email: DataTypes.STRING,
-        password_hash: DataTypes.STRING,
-    });
+	const User = sequelize.define(
+		"User",
+		{
+			name: DataTypes.STRING,
+			email: DataTypes.STRING,
+			password_hash: DataTypes.STRING,
+		},
+		{
+			hooks: {
+				beforeSave: async (user) => {
+					user.password_hash = "123";
+				},
+			},
+		}
+	);
 
-    return User;
+	return User;
 };

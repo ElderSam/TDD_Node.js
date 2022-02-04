9ac852641ea748dac044133c5a0efc17cb6451d5
ElderSam <eldersamuel98@gmail.com>
2022-02-03 21:03:01 -0300

    feat: add Authentication test (route /sessions)

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,3 +1,7 @@
+const request = require("supertest");
+
+const app = require("../../src/app");
+const { User } = require("../../src/app/models");
 const truncate = require("../utils/truncate")
 
 describe("Authentication", () => {
@@ -5,7 +9,20 @@ describe("Authentication", () => {
 		await truncate();
 	})
 
-	it("", async () => {
-		
+	it("should authenticate with valid credentials", async () => {
+		const user = await User.create({
+			name: "Diego",
+			email: "diego@rocketseat.com.br",
+			password_hash: "123123",
+		});
+
+		expect(user.email).toBe("diego@rocketseat.com.br")
+
+		const response = await request(app).post("/sessions").send({
+			email: user.email,
+			password: "123456",
+		});
+
+		expect(response.status).toBe(200);
 	});
 });
